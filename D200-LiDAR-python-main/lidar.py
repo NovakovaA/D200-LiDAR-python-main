efc8f53e6c176d95ac3d53489aad7f0df170af57
ale <aledeum.saf@gmail.com>
2023-12-19 15:22:01 -0300

    n

--- D200-LiDAR-python-main/lidar.py
+++ D200-LiDAR-python-main/lidar.py
@@ -160,6 +160,7 @@ if __name__ == "__main__":
                     filtered_data.append(d)
 
         for i, lm in enumerate(filtered_data):
+            if 360 > lm.angle > 0:
                 x.append(lm.distance * sin(math.radians(float(lm.angle))))
                 y.append(lm.distance * cos(math.radians(float(lm.angle))))
 
